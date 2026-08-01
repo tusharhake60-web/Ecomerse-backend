72cb1d82e29fe886f8d4dce5894a2294a52c562c
tushar <tusharhake60@gmail.com>
2026-08-01 16:53:01 +0530

    add cloudinary

---
+++ src/main/java/com/Config/CloudinaryConfig.java
@@ -0,0 +1,31 @@
+package com.Config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.cloudinary.Cloudinary;
+
+@Configuration
+public class CloudinaryConfig {
+
+    private String cloudName;
+
+    private String apiKey;
+
+    private String apiSecret;
+
+    @Bean
+    public Cloudinary cloudinary() {
+
+        Map<String, String> config = new HashMap<>();
+
+        config.put("cloud_name", cloudName);
+        config.put("api_key", apiKey);
+        config.put("api_secret", apiSecret);
+
+        return new Cloudinary(config);
+    }
+}
