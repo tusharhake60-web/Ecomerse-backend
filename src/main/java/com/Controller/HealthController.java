aa01380682d6cbb61dac64ce69837b3b56a6945f
tushar <tusharhake60@gmail.com>
2026-08-01 10:36:11 +0530

    port select

---
+++ src/main/java/com/Controller/HealthController.java
@@ -0,0 +1,15 @@
+package com.Controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+}
